5b0a4f6953a0e0978b53b6a553c9841babe673a7
amaraasp <amartuvshin@storepay.mn>
2023-12-07 16:20:35 +0800

    valid parentheses

---
+++ 20. Valid Parentheses.rb
@@ -0,0 +1,20 @@
+def is_valid(s)
+  stack = []
+  mapping = { ')' => '(', '}' => '{', ']' => '[' }
+
+  s.each_char do |char|
+    if mapping.key?(char)
+      top_element = stack.pop || '#'
+      return false if mapping[char] != top_element
+    else
+      stack.push(char)
+    end
+  end
+
+  stack.empty?
+end
+
+
+puts is_valid("()")         # Output: true
+puts is_valid("()[]{}")     # Output: true
+puts is_valid("(]")         # Output: false
